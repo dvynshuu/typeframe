bf4b03797f26be6d5cdc25da3502c5d72e91498c
dvynshuu <officialdivyanshu94@gmail.com>
2026-06-17 19:53:01 +0530

    feat: add Astro sitemap integration and update robots.txt for typeframe.pages.dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://typeframe.pages.dev',
   compressHTML: true,
+
   build: {
     inlineStylesheets: 'auto',
   },
+
   vite: {
     worker: {
       format: 'es',
     },
   },
+
+  integrations: [sitemap()],
 });
